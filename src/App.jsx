6c94a7a74bdcb8c0f05d6de00dc6cec5304cf585
SJ <127341132+minari314159@users.noreply.github.com>
2024-05-28 11:48:41 -0400

    Update screen component with suspense fallback and loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {
-  ScrollControls,
-  Scroll,
-  Environment,
-  OrbitControls,
+	ScrollControls,
+	Scroll,
+	Environment,
+	OrbitControls,
 } from "@react-three/drei";
 import DeskScene from "./DeskScene";
 
@@ -14,46 +14,45 @@ import LoadingScreen from "./LoadingScreen";
 import { useState, useEffect, Suspense } from "react";
 
 export default function App() {
-  const [section, changeSection] = useState(0);
-  const [open, setOpened] = useState(false);
-  const [started, setStarted] = useState(false);
+	const [section, changeSection] = useState(0);
+	const [open, setOpened] = useState(false);
+	const [started, setStarted] = useState(false);
 
-  useEffect(() => {
-    setOpened(false);
-  }, [section]);
-  return (
-    <>
-      <LoadingScreen started={started} setStarted={setStarted} />
-      <Menu changeSection={changeSection} open={open} />
-      <Canvas
-        shadows
-        dpr={[1, 2]}
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 1000,
-          position: [2, 1, 2],
-        }}
-      >
-        {/* <OrbitControls makeDefault /> */}
+	useEffect(() => {
+		setOpened(false);
+	}, [section]);
+	return (
+		<>
+			<LoadingScreen started={started} setStarted={setStarted} />
+			<Menu changeSection={changeSection} open={open} />
+			<Canvas
+				shadows
+				dpr={[1, 2]}
+				camera={{
+					fov: 45,
+					near: 0.1,
+					far: 1000,
+					position: [2, 1, 2],
+				}}>
+				{/* <OrbitControls makeDefault /> */}
 
-        {/* Scene */}
-        <Environment preset="night" />
-        <color args={["#000000"]} attach="background" />
-        <directionalLight castShadow position={[1.5, 2, 0]} intensity={1} />
+				{/* Scene */}
+				<Environment preset="night" />
+				<color args={["#000000"]} attach="background" />
+				<directionalLight castShadow position={[1.5, 2, 0]} intensity={1} />
 
-        <ScrollControls pages={3} distance={1} damping={0.5}>
-          <ScrollManager section={section} changeSection={changeSection} />
-          <Suspense>
-            {started && (
-              <DeskScene section={section} open={open} setOpened={setOpened} />
-            )}
-          </Suspense>
-          <Scroll position={[0, 0]} html>
-            {started && <Interface section={section} open={open} />}
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
-    </>
-  );
+				<ScrollControls pages={3} distance={1} damping={0.5}>
+					<ScrollManager section={section} changeSection={changeSection} />
+					<Suspense>
+						{started && (
+							<DeskScene section={section} open={open} setOpened={setOpened} />
+						)}
+					</Suspense>
+					<Scroll position={[0, 0]} html>
+						{started && <Interface section={section} open={open} />}
+					</Scroll>
+				</ScrollControls>
+			</Canvas>
+		</>
+	);
 }
--- src/Screen.jsx
+++ src/Screen.jsx
@@ -2,18 +2,15 @@ import { Suspense } from "react";
 import LoadingScreen from "./LoadingScreen";
 
 const Screen = () => {
-  return (
-    <section
-      className=" flex-3  h-[540px] w-[650px] lg:w-[780px] lg:h-[600px] outline-none border-none overflow-hidden screen bg-[rgb(113,63,18,0.1)] rounded-[4rem]"
-      onClick={(event) => {
-        event.stopPropagation();
-      }}
-    >
-      <Suspense fallback={null}>
-        <iframe overflow="none" src="https://3-d-portfolio-pi.vercel.app/" />
-      </Suspense>
-    </section>
-  );
+	return (
+		<section
+			className=" flex-3  h-[540px] w-[650px] lg:w-[780px] lg:h-[600px] outline-none border-none overflow-hidden screen bg-[rgb(113,63,18,0.1)] rounded-[4rem]"
+			onClick={(event) => {
+				event.stopPropagation();
+			}}>
+			<Suspense fallback={<LoadingScreen />}>Jokes</Suspense>
+		</section>
+	);
 };
 
 export default Screen;
